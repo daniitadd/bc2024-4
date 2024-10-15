18d36da986e2abf0e4cb9aa8087183ae81363ca4
daniitadd bc2024-4 <danadymchak@gmail.com>
2024-10-15 12:37:52 +0300

    Додано PUT

--- main.js
+++ main.js
@@ -36,6 +36,19 @@ const server = http.createServer(async (req, res) => {
         res.end('Internal Server Error');
       }
     }
+  } else if (req.method === 'PUT') {
+    let body = [];
+    req.on('data', chunk => body.push(chunk));
+    req.on('end', async () => {
+      try {
+        await fs.writeFile(filePath, Buffer.concat(body));
+        res.writeHead(201, { 'Content-Type': 'text/plain' });
+        res.end('Created');
+      } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+      }
+    });
   } else {
     res.writeHead(405, { 'Content-Type': 'text/plain' });
     res.end('Method Not Allowed');
